3ae426ca60c6799ea04dab011c5881dd30b369da
on3dd <onedeadwave@gmail.com>
2020-12-16 16:27:45 +1000

    fix

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,11 +2,7 @@ import { readFileSync } from 'fs';
 import R from 'ramda';
 
 export const read = (path: string) =>
-  R.pipe(
-    readFileSync,
-    R.toString,
-    R.split('\n'),
-  )(path);
+  R.pipe(readFileSync, R.toString, R.trim, R.split('\n'))(path);
 
 export const truthy = (value: unknown) =>
   R.complement(R.isNil)(value);
